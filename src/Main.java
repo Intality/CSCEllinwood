0d6f50a7b3c8aac4c0c3ac0750bb8489df5aa5b4
Intality <shawnellinwood@gmail.com>
2024-01-29 18:43:19 -0700

    Added TempConversion.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //First Program which is hello world
-        System.out.println("Hello, World!");
+
+               final double CM_PER_INCH =2.54;
+               final double KM_PER_INCH=3;
+                       Scanner in= new Scanner(System.in);
+                       System.out.println("Give me an amount in inches");
+                       int inches= in.nextInt();
+
+                       System.out.println(inches*CM_PER_INCH+ "centimeters");
+        System.out.println(inches*KM_PER_INCH+ "Kilometers");
         }
     }
---
+++ src/TempConversion.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TempConversion {
+    public static void main(String[] args) {
+
+        final double CM_PER_INCH =2.54;
+        final double KM_PER_INCH=3;
+        Scanner in= new Scanner(System.in);
+        System.out.println("Give me an amount in inches");
+        int inches= in.nextInt();
+
+        System.out.println(inches*CM_PER_INCH+ "centimeters");
+        System.out.println(inches*KM_PER_INCH+ "Kilometers");
+    }
+}
