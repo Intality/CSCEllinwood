835b8c748f974a4a9c55ee02963b2c74f335b488
Intality <156861381+Intality@users.noreply.github.com>
2024-01-30 23:04:51 -0700

    Update TempConversion.java
    Deleted the melting point equation.

--- TemperatureConversion/src/TempConversion.java
+++ TemperatureConversion/src/TempConversion.java
@@ -8,10 +8,7 @@ import java.util.Scanner;
 public class TempConversion {
     public static void main(String[] args) {
 
-        final double Melting_point_of_water =32;
-        //This is to set the melting or freezing point of water, so we get whatever °F that we input subtracted from 32
-
-        final double Fahrenheit_to_Celsius_Fraction= 0.556;
+        final double FAHRENHEIT_TO_CELSIUS_FRACTION= 0.556;
         //Because java does not understand fractions, or from what I have understood so far in this course, we must manually take 5/9 which equates to .555 repeatedly.
 //The more decimals you have, the more accurate the conversion will be, but not necessarily needed in this case.
 
@@ -24,14 +21,15 @@ final double Kelvin=273.15;
         //What this is doing is when the program is run, it is waiting for some input: some degree temperature Fahrenheit.
         //*NOTE: WE are not changing the "Give me a temperature in Fahrenheit" at all. This is simply a statement that is asking for some valid input when ran.
 
+        System.out.println((Fahrenheit-32)*(FAHRENHEIT_TO_CELSIUS_FRACTION)+ " Celsius");
+        //We are telling the program to take our input Fahrenheit, and give use the output as some converted temperature Celsius.
 
-        System.out.println((Fahrenheit-Melting_point_of_water)*Fahrenheit_to_Celsius_Fraction  + " Celsius");
-        //We are telling the program to take our formula of (°F - 32) × 5/9 and compute it.
-        //The computed result will print out with the temperature in degrees Celsius or °C.
 
-        System.out.println(((Fahrenheit-Melting_point_of_water)*Fahrenheit_to_Celsius_Fraction)+Kelvin+ " Kelvin");
+        System.out.println(((Fahrenheit-32)*FAHRENHEIT_TO_CELSIUS_FRACTION)+Kelvin+ " Kelvin");
 //Now that we have computed our temperature in Celsius, we are then telling the program to take that number and add 273.15.
         //From there, we are telling it to give us that computed number in degrees Kelvin!
+
+
 
     }
 }
